304145a0ad9bc804b071d240fb768dfa4d286932
gabi-elmaliah <gabbyelmaliah@gmail.com>
2024-04-10 12:32:16 +0100

    registration and authentication

---
+++ playground/flaskapp/website/views.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, render_template, request, flash, jsonify
+from flask_login import login_required, current_user
+
+views=Blueprint('views',__name__)
+
+@views.route('/')
+@login_required
+def home():
+    return render_template("home.html",user=current_user)
+@views.route('questionnaire')
+def questionnaire():
+     return render_template("questionnaire.html")
+
+
+
