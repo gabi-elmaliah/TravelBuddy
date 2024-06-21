64d8393adeff841582484a89d06f554622690728
gabi-elmaliah <gabbyelmaliah@gmail.com>
2024-06-21 11:44:22 +0100

    PlanTrip added

--- front/src/routes/PlanTrip.js
+++ front/src/routes/PlanTrip.js
@@ -1,16 +1,18 @@
-
-import  SearchBar  from "../components/SearchBar";
+import React, { useState } from 'react';
+import   SearchBar  from "../components/SearchBar";
+import SearchResultsList from '../components/SearchResultsList';
 import "./PlanTrip.css"
-import React from 'react';
+
 
 
 function PlanTrip(){
+    const [results, setResults] = useState([]);
 
     return(   
         <div className="PlanTrip">
             <div className="search-bar-container">
-                <SearchBar/>
-                <div>Results</div>
+                <SearchBar setResults={setResults} />
+                <SearchResultsList results={results} />
             </div>
           
         </div>
