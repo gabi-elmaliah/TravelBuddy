9dc5f31707602d6529444d52d65bdf17ec250817
gabi-elmaliah <gabbyelmaliah@gmail.com>
2024-06-21 11:43:19 +0100

    search bar added

--- front/src/components/SearchBar.js
+++ front/src/components/SearchBar.js
@@ -1,35 +1,44 @@
 import React,{useState} from 'react'
 import { FaSearch } from "react-icons/fa";
+import cities from "../cities.js"
 import "./SearchBar.css"
-import axios from 'axios';
 
 
-const SearchBar=()=> 
-    {
+const SearchBar=({setResults})=> 
+  {
     const [input,setInput]=useState("");
 
 
-    const fetchData = (value) => 
-    {
-
-        axios.get('http/v1/geo/places?limit=5&offset=0?namePrefix={value}')
-        .then(res=>console.log(res.data))
+    const fetchData = (value) => {
+      if (!value) {
+        setResults([]);
+        return;
+      }
+  
+      const lowercasedValue = value.toLowerCase();
+      const results = cities.filter(city =>
+        city.name.toLowerCase().includes(lowercasedValue)
+      );
+      setResults(results);
+    };
+    const handleChange = (value) => {
+      setInput(value);
+      fetchData(value);
     };
 
-    const handleChange=(value)=>{
-        setInput(value)
-        fetchData(value)
-    };
-    
+
+    return (
+      <div className="input-wrapper">
+        <FaSearch id="search-icon" />
+        <input
+          placeholder="Type to search..."
+          value={input}
+          onChange={(e) => handleChange(e.target.value)}
+        />
+      </div>
+    );
 
 
-
-  return (
-    <div className='input-wrapper'>
-           <FaSearch id="search-icon" />
-           <input placeholder="select a city" value={input} onChange={(e)=>setInput(e.target.value)} />
-    </div>
-  );
 };
-
+  
 export default SearchBar
